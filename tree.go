0c373a8e4cb3270a4d36df77ab2d0f4b3f5e11e8
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-02-08 16:04:03 +0500

    tree to cdll

--- tree.go
+++ tree.go
@@ -703,3 +703,47 @@ func isHeapUtil(curr *Node, parentValue int) bool {
 }
 
 
+
+//====================================================================================================================
+
+
+func (t *Tree)TreeToListRec() *Node{
+  return treeToListRec(t.root)
+}
+
+  
+func treeToListRec(curr *Node) *Node {
+  if curr == nil{
+      return nil
+  }
+  var Head, Tail, tempHead *Node
+
+  if curr.left == nil && curr.right == nil{
+      curr.left = curr
+      curr.right = curr
+      return curr
+  }
+
+  if curr.left != nil {
+      Head = treeToListRec(curr.left)
+      Tail = Head.left
+      curr.left = Tail
+      Tail.right = curr
+  } else {
+      Head = curr
+  }
+
+  if curr.right != nil {
+      tempHead = treeToListRec(curr.right)
+      Tail = tempHead.left
+      curr.right = tempHead
+      tempHead.left = curr
+  } else {
+      Tail = curr
+  }
+  Head.left = Tail
+
+  Tail.right = Head
+  return Head
+}
+
