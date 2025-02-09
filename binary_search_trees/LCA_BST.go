84b2250ebee76009d7664ce5dff58466002962cb
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-02-09 11:12:41 +0500

    lca bst

---
+++ binary_search_trees/LCA_BST.go
@@ -0,0 +1,18 @@
+func (t *Tree) LcaBST(first int, second int) (int, bool) {
+    return LcaBST(t.root, first, second)
+}
+
+func LcaBST(curr *Node, first int, second int) (int, bool) {
+    //Implement your solution here
+    if curr == nil {
+      return 0, false
+    }
+    if first < curr.value && second < curr.value {
+      return LcaBST(curr.left, first, second)
+    } else if first > curr.value && second > curr.value {
+      return LcaBST(curr.right, first, second)
+    } else {
+      return curr.value, true
+    }
+    return 0, false
+}
